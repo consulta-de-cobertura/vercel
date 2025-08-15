26ee44fba58fc7d798da1aff4a6df050308eb260
netfamilia683-star <netfamilia683@gmail.com>
2025-08-15 05:32:45 -0300

    Mover seção PBI para modal de cadastro

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+@@ .. @@
+         {/* 8. PLANS - Momento da oferta (quando já estão convencidos) */}
+         <Plans onRedirect={handleDirectRedirect} />
+         
+-        {/* 8.5. PBI SECTION - Programa de Indicação para transformar internet em renda */}
+-        <PBISection onRedirect={handleDirectRedirect} />
+-        
+         {/* 9. FAQ - Últimas dúvidas antes da conversão */}
+         <FAQ />
